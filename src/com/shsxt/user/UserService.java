408fc527e166c03d6282724994b05e0bf4f2f745
RayNorth <xiayuanhaonan@163.com>
2018-06-28 23:09:07 +0800

    第二次提交

---
+++ src/com/shsxt/user/UserService.java
@@ -0,0 +1,12 @@
+package com.shsxt.user;
+
+/**
+ * Created by Administrator on 2018/6/28.
+ */
+public class UserService {
+    public static void main(String[] args){
+    System.out.println("hello");
+        System.out.println("world");
+    }
+
+}
